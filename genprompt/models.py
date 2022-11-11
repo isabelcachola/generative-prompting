f644619dbe24373b60ab88a7764bb43268a15083
Isabel Cachola <cachola.isabel@gmail.com>
2022-11-11 18:58:22 -0500

    started gpt inference code, added lead 3 model

--- genprompt/models.py
+++ genprompt/models.py
@@ -1,32 +1,49 @@
 '''
 Loads and interacts with each model 
 '''
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizerFast
+from transformers import GPTJForCausalLM
+from transformers import pipeline
+from typing import List
+import spacy
+from datasets import load_dataset
+import pandas as pd
 
-class Model:
+class Lead3:
     def __init__(self):
-        pass
+        self.nlp = spacy.load('en_core_web_sm')
+
+    def generate_from_prompts(self, examples):
+        results = []
+        for ex in examples:
+            this_ex = []
+            text = ex.split('\n')[0]
+            tokens = self.nlp(text)
+            for i, s in enumerate(tokens.sents):
+                this_ex.append(s.text)
+                if i==2:
+                    break
+            results.append(' '.join(this_ex))
+        return results
+
     
-class GPTJ:
-    def __init__(self):
+class GPT:
+    def __init__(self, max_length, model_name):
+        self.generator = pipeline('text-generation', model=model_name)
+        self.max_length = max_length
 
-        self.model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
-        self.tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
-    
-    def test_prompt(self):
-        prompt = (
-            "In a shocking finding, scientists discovered a herd of unicorns living in a remote, "
-            "previously unexplored valley, in the Andes Mountains. Even more surprising to the "
-            "researchers was the fact that the unicorns spoke perfect English."
-        )
+    def generate_from_prompts(self, examples: List[str]) -> List[str]:
+        n_examples = len(examples)
+        output = []
+        for ex in examples:
+            generations = self.generator(ex, max_length=self.max_length, pad_token_id=50256, 
+                                        num_return_sequences=1, return_full_text=False, 
+                                        handle_long_generation="hole")
+            output += generations[0]['generated_text']
+        return output
 
-        input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
-
-        gen_tokens = self.model.generate(
-            input_ids,
-            do_sample=True,
-            temperature=0.9,
-            max_length=100,
-        )
-        gen_text = self.tokenizer.batch_decode(gen_tokens)[0]
-        return gen_text
+    def test_prompt(self) -> List[str]:
+        dataset = pd.read_csv('triviaqa.sample', header=None)
+        examples = dataset[0].values.tolist()
+        output = self.generate_from_prompts(examples)
+        return output
