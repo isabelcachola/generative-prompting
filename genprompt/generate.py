777f424f5bd040398709ecd14e4053e156b52600
Isabel Cachola <cachola.isabel@gmail.com>
2022-12-21 13:27:03 -0500

    change the encoding, made an argument for the output file, and changed the max tokens for the tokenizer

--- genprompt/generate.py
+++ genprompt/generate.py
@@ -3,15 +3,22 @@ import torch
 from tqdm import tqdm 
 import json
 import argparse
+import time
+
+MAX_NEW_TOKENS=80
 
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    tokenizer = AutoTokenizer.from_pretrained(args.model, padding_side="left")
+    tokenizer = AutoTokenizer.from_pretrained(args.model, padding_side="left", 
+                    truncation_side="left", truncation="max_length", max_length=2048-MAX_NEW_TOKENS)
     tokenizer.add_special_tokens({'pad_token': tokenizer.eos_token})
-    model = AutoModelForCausalLM.from_pretrained(args.model, revision="float16", torch_dtype=torch.float16).to(device)
+    if args.model == "EleutherAI/gpt-neo-1.3B":
+        model = AutoModelForCausalLM.from_pretrained(args.model).to(device)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(args.model, revision="float16", torch_dtype=torch.float16).to(device)
 
     lines = None
-    with open(args.input_file) as f:
+    with open(args.input_file, encoding='utf-8') as f:
         lines = json.load(f)
 
     lines_length = len(lines)
@@ -19,27 +26,39 @@ def main(args):
     batch_sz = args.batch
     i = 0
     with torch.no_grad():
+        fout = open(args.output_file, 'w')
+        pbar = tqdm(total=lines_length, ncols=0, disable=not args.verbose)
         while i < lines_length:
             prompt_batch = line_list[i:i+batch_sz]
-            toked = tokenizer(prompt_batch, return_tensors='pt', padding=True, truncation=True).to(device)
+            toked = tokenizer(prompt_batch, return_tensors='pt', padding=True, truncation=True, max_length=2048-MAX_NEW_TOKENS).to(device)
             tokenizer.pad_token = tokenizer.eos_token
+            
             gen_tokens = model.generate(
                 **toked,
                 do_sample=False,
-                max_new_tokens=80,
+                max_new_tokens=MAX_NEW_TOKENS,
             )
 
             gen_text = tokenizer.batch_decode(gen_tokens[:,toked.input_ids.shape[1]:], skip_special_tokens=True)
             for line in gen_text:
-                print(line.split('\n')[0].strip())
+                fout.write(line.replace('\n', ' ').strip() + '\n')
+                # print(line.split('\n')[0].strip())
+
             i+=batch_sz
+            pbar.update(batch_sz)
+        fout.close()
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, required=False)
     parser.add_argument("--input_file", type=str)
+    parser.add_argument("--output_file", type=str)
     parser.add_argument("--batch", type=int, default=8)
+    parser.add_argument("--verbose", default=False, action='store_true')
     args = parser.parse_args()
+    start = time.time()
     main(args)
+    end = time.time()
+    print(f'Time to run script = {(end-start)/60} mins')
 
